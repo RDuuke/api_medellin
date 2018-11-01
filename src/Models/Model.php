54219b2eb8936afa9890a343c40ec3051851dd6e
rduuke <juuanduuke@gmail.com>
2018-11-01 16:06:09 -0500

    git ignore funciona

--- src/Models/Model.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Api\Models;
-
-use Illuminate\Database\Eloquent\Model as M;
-
-
-abstract class Model extends M{
-    public $timestamps = false;
-}
--- src/Models/UsersApi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Api\Models;
-
-
-class UsersApi extends Model
-{
-    protected $table = "users_api";
-}
