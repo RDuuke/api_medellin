20b7e2aa944b41b95784b69be65117b6965eb6b0
root <root@platformjml.arrobamedellin.edu.co>
2019-08-29 13:34:39 -0500

    reset

--- config/database.php
+++ config/database.php
@@ -6,9 +6,9 @@ return [
         'db' => [
             'driver' => 'mysql',
             'host' => 'localhost',
-            'database' => 'api_movil',
-            'username' => 'root',
-            'password' => '',
+            'database' => 'dbarrobapp',
+            'username' => 'arroba',
+            'password' => 'MXQ53105pn',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
